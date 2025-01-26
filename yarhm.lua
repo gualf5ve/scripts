8ff95514220bdafdecf8f4d833152e6e3cafd74f
gualf5ve <itzm3gap0n@gmail.com>
2025-01-26 13:32:23 +0300

    Update yarhm.lua

--- yarhm.lua
+++ yarhm.lua
@@ -1,1 +1,1 @@
-https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.18/yarhm.lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.18/yarhm.lua"))()
